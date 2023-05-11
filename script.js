c9044501c43e45fbd35d7dcead3fb653910ef2d2
Vanshika Raj Sharma <51854053+rsvanshika@users.noreply.github.com>
2023-05-11 10:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+
+let buttonEl=document.getElementByid("button");
+
+
+function getInfo() {
+	let nameEl=document.getElementByid("name");
+   let numEl=document.getElementByid("year");
+	let hEl=document.getElementById("head");
+	alert(nameEL.value)
+
+	// hEl+= nameEl.value + "and" + year.value ;
+	
+	
+}
+buttonEl.addEventLister("click",getInfo);
+
